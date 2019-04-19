2725b3a8be33ca57bbb064dbf3b7ec210a7ec101
Vlad <pokidin@ram.com.ua>
2019-04-19 17:15:14 +0300

    init2

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const IndexPage = () => (
+	<div>
+		<div className="Hero">
+			<div className="HeroGroup">
+				<h1>Learn to design and code React Apps!</h1>
+				<p>Complete courses about the bese toosl and design systems. Prototypes adn build apps with love and react!</p>
+				<Link to="/page-2/">Watch the video</Link>
+			</div>
+		</div>
+	</div>
+)
+
+export default IndexPage
